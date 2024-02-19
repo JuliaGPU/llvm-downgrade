3f1e74b8f2851e21f661ff7bb24bb967b5eb91d8
Tim Besard <tim.besard@gmail.com>
2024-02-19 20:00:46 +0100

    Add support for writing LLVM 5.0 bitcode.
    (cherry picked from commit ad1b8a2a1c5541a4390d09438310c3994c29ebfa)

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -313,6 +313,7 @@ void initializeVirtRegRewriterPass(PassRegistry&);
 void initializeWasmEHPreparePass(PassRegistry&);
 void initializeWinEHPreparePass(PassRegistry&);
 void initializeWriteBitcodePassPass(PassRegistry&);
+void initializeWriteBitcodePass50Pass(PassRegistry&);
 void initializeXRayInstrumentationPass(PassRegistry&);
 
 } // end namespace llvm
