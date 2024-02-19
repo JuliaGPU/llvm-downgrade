774b5052b5f109106e9991a8ef3788be8379169d
Tim Besard <tim.besard@gmail.com>
2024-02-19 20:00:46 +0100

    Add support for writing LLVM 5.0 bitcode.
    (cherry picked from commit bcab72394e46cb3df1c173aa04ee3afb6d918d69)

--- llvm/include/llvm/Bitcode/BitcodeWriterPass.h
+++ llvm/include/llvm/Bitcode/BitcodeWriterPass.h
@@ -33,6 +33,11 @@ LLVM_ABI ModulePass *
 createBitcodeWriterPass(raw_ostream &Str,
                         bool ShouldPreserveUseListOrder = false);
 
+ModulePass *createBitcode50WriterPass(raw_ostream &Str,
+                                      bool ShouldPreserveUseListOrder = false,
+                                      bool EmitSummaryIndex = false,
+                                      bool EmitModuleHash = false);
+
 /// Check whether a pass is a BitcodeWriterPass.
 LLVM_ABI bool isBitcodeWriterPass(Pass *P);
 
@@ -66,6 +71,32 @@ public:
   LLVM_ABI PreservedAnalyses run(Module &M, ModuleAnalysisManager &);
 
   static bool isRequired() { return true; }
+};
+
+class BitcodeWriterPass50 : public PassInfoMixin<BitcodeWriterPass50> {
+  raw_ostream &OS;
+  bool ShouldPreserveUseListOrder;
+  bool EmitSummaryIndex;
+  bool EmitModuleHash;
+
+public:
+  /// Construct a bitcode writer pass around a particular output stream.
+  ///
+  /// If \c ShouldPreserveUseListOrder, encode use-list order so it can be
+  /// reproduced when deserialized.
+  ///
+  /// If \c EmitSummaryIndex, emit the summary index (currently
+  /// for use in ThinLTO optimization).
+  explicit BitcodeWriterPass50(raw_ostream &OS,
+                               bool ShouldPreserveUseListOrder = false,
+                               bool EmitSummaryIndex = false,
+                               bool EmitModuleHash = false)
+      : OS(OS), ShouldPreserveUseListOrder(ShouldPreserveUseListOrder),
+  EmitSummaryIndex(EmitSummaryIndex), EmitModuleHash(EmitModuleHash) {}
+
+  /// Run the bitcode writer pass, and output the module to the selected
+  /// output stream.
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &);
 };
 
 }
